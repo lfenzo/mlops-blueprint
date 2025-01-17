6358dcce16ea2672df9e1ebfa6effc3aa8fd6554
lfenzo <lfenzo@protonmail.com>
2025-01-17 11:17:00 -0300

    introduced docker in docker to build model images from mlflow registry

--- airflow/dags/model_inference.py
+++ airflow/dags/model_inference.py
@@ -13,6 +13,7 @@ with DAG(
 
     generate_dockerfile = DockerOperator(
         task_id='generate-dockerfile',
+        auto_remove="success",
         image='ghcr.io/mlflow/mlflow',
         command="""
             sh -c '
@@ -30,41 +31,35 @@ with DAG(
         mounts=[Mount(source="/tmp", target="/tmp", type="bind")]
     )
 
-    build_docker_image = DockerOperator(
-        task_id='build-docker-image',
-        image='docker:latest',
-        command="sh -c 'docker build -t mlflow-best-model /tmp/model'",
-        host_tmp_dir="/tmp",
-        mounts=[
-            Mount(source="/var/run/docker.sock", target="/var/run/docker.sock", type="bind"),
-            Mount(source="/tmp", target="/tmp", type="bind")
-        ]
-    )
-
-    # this needs to be address with docker in docker
-    # currently the spawed container is not able to resolve 'mlflow-nexus:8082'
-    # even though thery are int he same network
-    push_image_to_registry = DockerOperator(
-        task_id="push-docker-image-to-registry",
-        image="docker:latest",
+    build_image = DockerOperator(
+        task_id="build-docker-image",
+        image="docker:dind",
+        privileged=True,
+        auto_remove="success",
+        api_version="auto",
+        network_mode="mlops-blueprint_default",
+        environment={
+            "DOCKER_HOST": "unix:///var/run/docker.sock",
+        },
         command="""
             sh -c '
             DATE_TAG=$(date +%Y-%m-%d);
             IMAGE_NAME="mlflow-best-model";
-            REGISTRY="localhost:8082";  # must be changed to "mlflow-nexus" in the future
+            REGISTRY="http://mlflow-nexus:8082";
 
-            docker tag $REGISTRY/$IMAGE_NAME:latest $REGISTRY/$IMAGE_NAME:$DATE_TAG;
+            dockerd --insecure-registry $REGISTRY & sleep 10;
 
-            docker login http://localhost:8082 -u airflow -p airflow;
+            docker build -t $IMAGE_NAME /tmp/model;
+            docker tag $IMAGE_NAME $REGISTRY/$IMAGE_NAME:latest;
+            docker tag $IMAGE_NAME $REGISTRY/$IMAGE_NAME:$DATE_TAG;
+            docker images;
 
+            docker login http://$REGISTRY -u airflow -p airflow;
             docker push $REGISTRY/$IMAGE_NAME:latest;
             docker push $REGISTRY/$IMAGE_NAME:$DATE_TAG;
         '""",
-        network_mode="mlops-blueprint_default",
-        mounts=[
-            Mount(source="/var/run/docker.sock", target="/var/run/docker.sock", type="bind"),
-            Mount(source="/tmp", target="/tmp", type="bind")
-        ]
+        host_tmp_dir="/tmp",
+        mounts=[Mount(source="/tmp", target="/tmp", type="bind")],
     )
 
-    generate_dockerfile >> build_docker_image >> push_image_to_registry
+    generate_dockerfile >> build_image
